728d6e6158050659143bd6160ecac4c5ed4980b5
Bilişim Çalışanları Dayanışma Ağı <das@emrey.pw>
2014-09-12 14:03:59 +0300

    sorumlu.c
    Sorumlu buldurucu. array degiskenini niye tanimladigimi soranin annnasina soverim.

---
+++ bul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char array[100];
+
+    printf("Sorumlusunu bulmak istediginiz sorunu giriniz\n>");
+    scanf("%s", array);
+    printf("Sorumlu aranıyor...\n");
+    sleep(2);
+    printf("SORUMLU BULUNDU!!!!11!!1bir\n\n\n");
+    sleep(2);
+    printf("Sorumlu: Recep Tayyip Erdogan\n");
+    return 0;
+}
